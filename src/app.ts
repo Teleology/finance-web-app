a6b037d8a7228e9bab73960d6ceaf4c163a06365
tang Xiao <tangxiao933@gmail.com>
2020-06-19 22:28:30 -0700

    add static middle

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import path from 'path';
 import express, { Application, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import { Container } from 'inversify';
@@ -13,5 +14,6 @@ server.setConfig((app: Application) => {
   app.use(express.json());
   app.use(urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 server.build().listen(3000, () => console.log('Server is not listening on port 3000'));
