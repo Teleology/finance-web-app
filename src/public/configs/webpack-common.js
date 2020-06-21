781a0106f628627d3e5d43c6a2c8affe037d0748
tang Xiao <tangxiao933@gmail.com>
2020-06-20 17:41:41 -0700

    add webpack

---
+++ src/public/configs/webpack-common.js
@@ -0,0 +1,59 @@
+// shared config (dev and prod)
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  context: resolve(__dirname, '../../src'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader', 'source-map-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, { loader: 'sass-loader', options: { includePaths: ['node_modules'] } }],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 7,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+  performance: {
+    hints: false,
+  },
+};
---
+++ src/public/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const rootEl = document.querySelector('#root');
+
+render(<h1>123456</h1>, rootEl);
