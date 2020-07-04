b2ef82adc7498d71b9031f39710087ff77f5f04a
Xiao Tang <tangxiao933@gmail.com>
2020-07-04 01:04:58 -0700

    before adding axios

--- express-server/src/stock-data/stock-data.controller.ts
+++ express-server/src/stock-data/stock-data.controller.ts
@@ -1,12 +1,14 @@
 import { encode } from 'querystring';
 import { Request, Response } from 'express';
 import { controller, httpGet } from 'inversify-express-utils';
+// eslint-disable-next-line import/no-unresolved
 import { Params } from 'express-serve-static-core';
+import axios from 'axios';
 import { StockQueryType } from '../typings/stock-query.type';
 @controller('/api/v1/stock')
 export class StockDataController {
   @httpGet('/query')
-  public getStockInfo(req: Request<Params, any, any, StockQueryType>, res: Response): void {
+  public getStockInfo(req: Request<Params, never, never, StockQueryType>, res: Response): void {
     try {
       console.log(encode(req.query));
       res.status(200).send('good data');
