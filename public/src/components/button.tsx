649cf10afbc24b782499d8ed48bc20d929c4ac4c
Xiao Tang <tangxiao933@gmail.com>
2020-08-25 01:16:02 -0700

    before fp

--- public/src/components/button.tsx
+++ public/src/components/button.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import { get } from 'lodash';
 import styled, { DefaultTheme } from 'styled-components';
 
 const Button: React.FC<{}> = styled.button`
   padding: 8px 16px;
-  background-color: ${(props: { theme: DefaultTheme }): string => props.theme.palette.primary};
+  background-color: ${(props: { theme: DefaultTheme }): string => get<DefaultTheme, 'palette', 'primary'>(props.theme, ['palette', 'primary'])};
   color: snow;
   border: 0;
   border-radius: 0.2rem;
