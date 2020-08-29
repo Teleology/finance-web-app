55a3bf2b224c17eb10803012af3470b44340e7f2
Xiao Tang <tangxiao933@gmail.com>
2020-08-29 00:07:00 -0700

    disable esModuleInterop

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Route, Switch } from 'react-router';
@@ -30,17 +30,15 @@ const d = (): React.ReactElement => (
   </h1>
 );
 
-export const App = (): React.ReactElement => {
-  return (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact={true} path={'/user1'} render={a} />
-          <Route exact={true} path="/user2" render={b} />
-          <Route exact={true} path="/user3" render={c} />
-          <Route exact={true} path="*" render={d} />
-        </Switch>
-      </ConnectedRouter>
-    </Provider>
-  );
-};
+export const App = (): React.ReactElement => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact={true} path={'/user1'} render={a} />
+        <Route exact={true} path="/user2" render={b} />
+        <Route exact={true} path="/user3" render={c} />
+        <Route exact={true} path="*" render={d} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+);
