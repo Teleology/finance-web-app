bfe73aac066189645627e1a1a5ecacf6813bb207
Xiao Tang <tangxiao933@gmail.com>
2020-08-30 00:18:51 -0700

    remove style components , add material ui

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -1,48 +1,19 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import { Route, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import { Container, ThemeProvider } from '@material-ui/core';
 import { store, history } from './root-redux/root-store';
-import { Button } from './components/button';
-import { GlobalStyle, theme } from './theme';
-
-const a = (): React.ReactElement => (
-  <h1>
-    React App
-    <Link to={'/b'}>To B</Link>
-  </h1>
-);
-const b = (): React.ReactElement => (
-  <h1>
-    React Bpp
-    <Link to={'/c'}>To C</Link>
-  </h1>
-);
-const c = (): React.ReactElement => (
-  <h1>
-    React Cpp
-    <Link to={'/a'}>To A</Link>
-  </h1>
-);
-const d = (): React.ReactElement => (
-  <Button onClick={console.log} variant={'secondary'}>
-    123123123
-  </Button>
-);
+import { theme } from './theme';
+import { SelectionPanel } from './containers/selection-panel.component';
 
 export const App = (): React.ReactElement => (
-  <ThemeProvider theme={theme}>
-    <Provider store={store}>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
       <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact={true} path={'/user1'} render={a} />
-          <Route exact={true} path="/user2" render={b} />
-          <Route exact={true} path="/user3" render={c} />
-          <Route exact={true} path="*" render={d} />
-        </Switch>
+        <Container maxWidth="xl">
+          <SelectionPanel />
+        </Container>
       </ConnectedRouter>
-    </Provider>
-  </ThemeProvider>
+    </ThemeProvider>
+  </Provider>
 );
--- public/src/theme.ts
+++ public/src/theme.ts
@@ -1,285 +1,85 @@
-import { createGlobalStyle } from 'styled-components';
-
-const theme = {
+import { createMuiTheme, colors } from '@material-ui/core';
+const theme = createMuiTheme({
   palette: {
-    blue: '#1870dc',
-    indigo: '#6610f2',
-    purple: '#a700ae',
-    pink: '#e671b8',
-    red: '#f45722',
-    orange: '#f0af03',
-    yellow: '#ffc107',
-    green: '#58d777',
-    teal: '#4ebfbb',
-    cyan: '#17a2b8',
-    white: '#f4f4f5',
-    grayDark: '#3c484f',
-    primary: '#1870dc',
-    secondary: 'rgba(244,244,245,0.9)',
-    success: '#58d777',
-    info: '#4ebfbb',
-    warning: '#f0af03',
-    danger: '#f45722',
-    light: '#f8f9fa',
-    dark: '#495057',
-    inverse: '#30314e',
-    gray: '#595d78',
-    default: '#474d84',
-    primaryLight: '#dee4ee',
-    successLight: '#ecfaec',
-    infoLight: '#f2fafa',
-    warningLight: '#fdf7e6',
-    dangerLight: '#fff2ef'
-  },
-  fonts: {
-    tiny: {
-      fontFamily: "'Segoe UI', 'Segoe UI Web (West European)', 'Segoe UI', -apple-system, BlinkMacSystemFont, 'Roboto', 'Helvetica Neue', sans-serif",
-      MozOsxFontSmoothing: 'grayscale',
-      WebkitFontSmoothing: 'antialiased',
-      fontSize: '10px',
-      fontWeight: 400
+    background: {
+      default: colors.common.white,
+      paper: colors.common.white
     },
-    xSmall: {},
-    small: {},
-    smallPlus: {},
-    medium: {},
-    mediumPlus: {},
-    large: {},
-    xLarge: {
-      fontFamily: "'Segoe UI', 'Segoe UI Web (West European)', 'Segoe UI', -apple-system, BlinkMacSystemFont, 'Roboto', 'Helvetica Neue', sans-serif",
-      MozOsxFontSmoothing: 'grayscale',
-      WebkitFontSmoothing: 'antialiased',
-      fontSize: '20px',
-      fontWeight: 600
+    primary: {
+      main: colors.indigo[500]
     },
-    xLargePlus: {
-      fontFamily: "'Segoe UI', 'Segoe UI Web (West European)', 'Segoe UI', -apple-system, BlinkMacSystemFont, 'Roboto', 'Helvetica Neue', sans-serif",
-      MozOsxFontSmoothing: 'grayscale',
-      WebkitFontSmoothing: 'antialiased',
-      fontSize: '24px',
-      fontWeight: 600
+    secondary: {
+      main: colors.indigo[500]
+    },
+    text: {
+      primary: colors.blueGrey[900],
+      secondary: colors.blueGrey[600]
     }
   },
-  spacing: {
-    xs: 4,
-    s: 8,
-    m: 16,
-    l: 20,
-    xl: 32
-  },
-  shadow: {
-    elevation4: '0 1.6px 3.6px 0 rgba(0, 0, 0, 0.132), 0 0.3px 0.9px 0 rgba(0, 0, 0, 0.108)',
-    elevation8: '0 3.2px 7.2px 0 rgba(0, 0, 0, 0.132), 0 0.6px 1.8px 0 rgba(0, 0, 0, 0.108)',
-    elevation16: '0 6.4px 14.4px 0 rgba(0, 0, 0, 0.132), 0 1.2px 3.6px 0 rgba(0, 0, 0, 0.108)',
-    elevation64: '0 25.6px 57.6px 0 rgba(0, 0, 0, 0.22), 0 4.8px 14.4px 0 rgba(0, 0, 0, 0.18)'
+  shadows: [
+    'none',
+    '0 0 0 1px rgba(63,63,68,0.05), 0 1px 2px 0 rgba(63,63,68,0.15)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 2px 2px -2px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 3px 4px -2px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 3px 4px -2px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 6px -2px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 6px -2px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 8px -2px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 5px 8px -2px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 6px 12px -4px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 7px 12px -4px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 6px 16px -4px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 7px 16px -4px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 8px 18px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 9px 18px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 10px 20px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 11px 20px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 12px 22px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 13px 22px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 14px 24px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 16px 28px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 18px 30px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 20px 32px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 22px 34px -8px rgba(0,0,0,0.25)',
+    '0 0 1px 0 rgba(0,0,0,0.31), 0 24px 36px -8px rgba(0,0,0,0.25)'
+  ],
+  typography: {
+    h1: {
+      fontWeight: 500,
+      fontSize: 35,
+      letterSpacing: '-0.24px'
+    },
+    h2: {
+      fontWeight: 500,
+      fontSize: 29,
+      letterSpacing: '-0.24px'
+    },
+    h3: {
+      fontWeight: 500,
+      fontSize: 24,
+      letterSpacing: '-0.06px'
+    },
+    h4: {
+      fontWeight: 500,
+      fontSize: 20,
+      letterSpacing: '-0.06px'
+    },
+    h5: {
+      fontWeight: 500,
+      fontSize: 16,
+      letterSpacing: '-0.05px'
+    },
+    h6: {
+      fontWeight: 500,
+      fontSize: 14,
+      letterSpacing: '-0.05px'
+    },
+    overline: {
+      fontWeight: 500
+    }
   }
-};
+});
 
-export const GlobalStyle = createGlobalStyle`
-html {
-  line-height: 1.15; 
-  -webkit-text-size-adjust: 100%; 
-}
+console.log(theme);
 
-
-body {
-  margin: 0;
-}
-
-main {
-  display: block;
-}
-
-
-h1 {
-  font-size: 2em;
-  margin: 0.67em 0;
-}
-
-hr {
-  box-sizing: content-box; 
-  height: 0; 
-  overflow: visible; 
-}
-
-pre {
-  font-family: monospace, monospace; 
-  font-size: 1em; 
-}
- 
-
-a {
-  background-color: transparent;
-}
-
-
-abbr[title] {
-  border-bottom: none; 
-  text-decoration: underline; 
-  text-decoration: underline dotted; 
-}
-
-
-b,
-strong {
-  font-weight: bolder;
-}
-
-
-code,
-kbd,
-samp {
-  font-family: monospace, monospace; 
-  font-size: 1em; 
-}
-
-small {
-  font-size: 80%;
-}
-
-sub,
-sup {
-  font-size: 75%;
-  line-height: 0;
-  position: relative;
-  vertical-align: baseline;
-}
-
-sub {
-  bottom: -0.25em;
-}
-
-sup {
-  top: -0.5em;
-}
-
-img {
-  border-style: none;
-}
-
-button,
-input,
-optgroup,
-select,
-textarea {
-  font-family: inherit; 
-  font-size: 100%; 
-  line-height: 1.15; 
-  margin: 0; 
-}
-
-button,
-input { 
-  overflow: visible;
-}
-
-button,
-select { 
-  text-transform: none;
-}
-
-button,
-[type="button"],
-[type="reset"],
-[type="submit"] {
-  -webkit-appearance: button;
-}
-
-button::-moz-focus-inner,
-[type="button"]::-moz-focus-inner,
-[type="reset"]::-moz-focus-inner,
-[type="submit"]::-moz-focus-inner {
-  border-style: none;
-  padding: 0;
-}
-
-button:-moz-focusring,
-[type="button"]:-moz-focusring,
-[type="reset"]:-moz-focusring,
-[type="submit"]:-moz-focusring {
-  outline: 1px dotted ButtonText;
-}
-
-fieldset {
-  padding: 0.35em 0.75em 0.625em;
-}
-
-legend {
-  box-sizing: border-box; 
-  color: inherit; 
-  display: table; 
-  max-width: 100%; 
-  padding: 0; 
-  white-space: normal; 
-}
- 
-
-progress {
-  vertical-align: baseline;
-}
-
-
-textarea {
-  overflow: auto;
-}
-
-
-[type="checkbox"],
-[type="radio"] {
-  box-sizing: border-box; 
-  padding: 0; 
-}
-
-[type="number"]::-webkit-inner-spin-button,
-[type="number"]::-webkit-outer-spin-button {
-  height: auto;
-}
-
-
-[type="search"] {
-  -webkit-appearance: textfield; 
-  outline-offset: -2px; 
-}
-
-[type="search"]::-webkit-search-decoration {
-  -webkit-appearance: none;
-}
-
-::-webkit-file-upload-button {
-  -webkit-appearance: button; 
-  font: inherit; 
-}
-
-details {
-  display: block;
-}
-
-summary {
-  display: list-item;
-}
-
-template {
-  display: none;
-}
-
-[hidden] {
-  display: none;
-}
-`;
 export { theme };
-/*
-breakpoint-xs: 0;
-breakpoint-sm: 576px;
-breakpoint-md: 768px;
-breakpoint-lg: 992px;
-breakpoint-xl: 1200px;
-
-      fontFamily: "'Segoe UI', 'Segoe UI Web (West European)', 'Segoe UI', -apple-system, BlinkMacSystemFont, 'Roboto', 'Helvetica Neue', sans-serif",
-      MozOsxFontSmoothing: 'grayscale',
-      WebkitFontSmoothing: 'antialiased',
-      fontSize: '18px',
-      fontWeight: 400,
-
-      font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
-font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace
- */
