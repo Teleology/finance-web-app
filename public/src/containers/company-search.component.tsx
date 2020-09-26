7692cbf1a9b358bb71b8a2b30410b7a65cb5590b
Shaw Clinton <tangxiao933@gmail.com>
2020-09-26 14:26:16 -0700

    add search

--- public/src/containers/company-search.component.tsx
+++ public/src/containers/company-search.component.tsx
@@ -1,62 +1,64 @@
 import * as React from 'react';
 import { Observable, combineLatest } from 'rxjs';
 import { isEmpty, negate } from 'lodash/fp';
-import { mergeMap, pluck, filter, debounceTime, startWith } from 'rxjs/operators';
-import { useEventCallback } from 'rxjs-hooks';
+import { mergeMap, pluck, filter, debounceTime, startWith, map } from 'rxjs/operators';
+import { useEventCallback, useObservable } from 'rxjs-hooks';
 import { TextField } from '@material-ui/core';
 import { ajax } from 'rxjs/ajax';
 import { stringifyUrl } from 'query-string';
+import { Autocomplete, AutocompleteRenderInputParams } from '@material-ui/lab';
 import { LabelText } from '../utils/type-util';
 import { baseURL } from '../../../express-server/src/common/network-utils';
 
-// TODO: consider extraction
-type Company = LabelText<string>;
+type Company = {
+  symbol: string;
+  name: string;
+  type: string;
+  region: string;
+  marketOpen: string;
+  marketClose: string;
+  timezone: string;
+  currency: string;
+  matchScore: string;
+};
 
-// const CompanySearch = (): React.ReactElement => {
-//   const [value, setValue] = React.useState<Company>({ value: 'a', label: 'labelA' });
-//   const [options, setOptions] = React.useState<Array<Company>>([
-//     { value: 'abc', label: 'abc' },
-//     { value: 'bcd', label: 'bcd' },
-//     { value: 'cdf', label: 'cdf' },
-//     { value: 'dfe', label: 'dfe' }
-//   ]);
-//   const getOptionLabel = React.useCallback((company: Company) => company.label, []);
-//   const getOptionSelected = React.useCallback((option: Company, value: Company) => option.value === value.value, []);
-//   const renderInput = React.useCallback(
-//     (params: AutocompleteRenderInputParams): React.ReactElement => <TextField {...params} label="Combo box" variant="outlined" />,
-//     []
-//   );
-//
-//   // const onChange = React.useCallback(
-//   //   (event: React.ChangeEvent<HTMLInputElement>) => {
-//   //     const { value } = event.target;
-//   //     setValue(value);
-//   //   },
-//   //   [setValue]
-//   // );
-//   return <Autocomplete<Company> renderInput={renderInput} options={options} getOptionLabel={getOptionLabel} getOptionSelected={getOptionSelected} />;
-// };
-// const reducer = (input: string)
 const CompanySearch = (): React.ReactElement => {
-  const [changeCallBack, [value, options]] = useEventCallback(
-    (event$: Observable<React.ChangeEvent<HTMLInputElement>>) => {
-      const keyword$ = event$.pipe(pluck('target', 'value'));
-      const response$ = keyword$.pipe(
+  const [input, setInput] = React.useState('');
+  const onInputChange = React.useCallback(
+    (event: object, value: string) => {
+      setInput(value);
+    },
+    [setInput]
+  );
+  const options = useObservable(
+    (noUse: Observable<unknown>, input$: Observable<[string]>) => {
+      const response$ = input$.pipe(
+        map((input: [string]) => input[0]),
         filter(negate(isEmpty)),
         debounceTime(1000),
-        mergeMap((keywords: string) => ajax.getJSON(stringifyUrl({ url: `${baseURL}/search`, query: { keywords } }))),
-        startWith([])
+        mergeMap((keywords: string) => ajax.getJSON<Array<Company>>(stringifyUrl({ url: `${baseURL}/search`, query: { keywords } })))
       );
-      return combineLatest([keyword$, response$]);
+      return response$;
     },
-    ['', []]
+    [],
+    [input]
+  );
+
+  const getOptionLabel = React.useCallback((company: Company) => company.name, []);
+  const getOptionSelected = React.useCallback((option: Company, value: Company) => option.symbol === value.symbol, []);
+  const renderInput = React.useCallback(
+    (params: AutocompleteRenderInputParams): React.ReactElement => <TextField {...params} label="Combo box" variant="outlined" />,
+    []
   );
 
   return (
-    <div>
-      <p>{JSON.stringify(options)}</p>
-      <TextField onChange={changeCallBack} value={value} />
-    </div>
+    <Autocomplete<Company>
+      renderInput={renderInput}
+      options={options}
+      onInputChange={onInputChange}
+      getOptionLabel={getOptionLabel}
+      getOptionSelected={getOptionSelected}
+    />
   );
 };
 export { CompanySearch };
