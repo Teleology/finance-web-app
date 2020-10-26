0396b242a7fd77f17a808188be26842c9d9b7d0c
Shaw Clinton <tangxiao933@gmail.com>
2020-10-25 20:46:41 -0700

    rename to compoennt

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -6,14 +6,14 @@ import { Route, Switch } from 'react-router';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, history, persistor } from './service/root-store';
 import { theme } from './theme';
-import { CompanyWatcher } from './ui/middle-level/company-watcher.component';
-import { StockTimeSeriesChartContainer } from './ui/middle-level/time-series-chart/stock-time-series-chart.component';
-import { NewsSectionContainer } from './ui/middle-level/news/news-section.component';
-import { AppHeader } from './ui/middle-level/app-header/app-header.component';
-import { AppDrawer } from './ui/middle-level/app-drawer/app-drawer.component';
-import { AppDrawerManager } from './ui/middle-level/app-drawer-manager.component';
+import { CompanyWatcher } from './component/middle-level/company-watcher.component';
+import { StockTimeSeriesChartContainer } from './component/middle-level/time-series-chart/stock-time-series-chart.component';
+import { NewsSectionContainer } from './component/middle-level/news/news-section.component';
+import { AppHeader } from './component/middle-level/app-header/app-header.component';
+import { AppDrawer } from './component/middle-level/app-drawer/app-drawer.component';
+import { AppDrawerManager } from './component/middle-level/app-drawer-manager.component';
 import { compareLink, infoLink, searchLink } from './utils/network-util';
-import { SearchPage } from './ui/top-level/search-page.component';
+import { SearchPage } from './component/top-level/search-page.component';
 
 export const App = (): React.ReactElement => (
   <Provider store={store}>
