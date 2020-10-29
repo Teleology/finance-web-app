7c446339261d058fb9da583a05e6fa2f7cd04cd1
Shaw Clinton <tangxiao933@gmail.com>
2020-10-28 22:06:52 -0700

    test

--- public/src/app.tsx
+++ public/src/app.tsx
@@ -2,31 +2,39 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Route, Switch } from 'react-router';
+import {  } from 'react-linkify'
 import { Link } from 'react-router-dom';
 // @ts-ignore
 import * as urlRegex from 'url-regex-safe';
 import { store, history } from './root-redux/root-store';
+const regex: RegExp = urlRegex();
 
 // console.log(urlRegex().source);
-// const matchesArray = 'GMAP_2018_HT2_10000.arw'.split(urlRegex());
-// const matchesArray2 = 'GMAP_2018_HT2_10000.arw'.match(new RegExp('(' + urlRegex().source + ')'));
-const regex: RegExp = urlRegex({ exact: true });
-console.log(regex.test('GMAP_2018_HT2_10000.ar'));
-console.log('GMAP_2018_HT2_10000.arw'.match(regex));
+const matchesArray = 'GMAP_2018_HT2_10000.arw'.match(urlRegex());
+const matchesArray2 = 'GMAP_2018_HT2_10000.arw'.match(new RegExp(`(${regex.source})`));
+const matchesArray3 = 'GMAP_2018_HT2_10000.arw'.match(new RegExp('(\\b' + regex.source + '\\b)'));
+const mathesArray4 = 'go to    www.test.com and http://www.testurl.com'.match(new RegExp('(\\b' + regex.source + '\\b)'));
+console.log(matchesArray);
+console.log(matchesArray2);
+console.log(matchesArray3);
+console.log(mathesArray4);
+
+// console.log(regex.test('GMAP_2018_HT2_10000.ar'));
+// console.log('GMAP_2018_HT2_10000.arw'.match(regex));
 // console.log(matchesArray, matchesArray2);
 
 // const matchesArray = 'github.comfoobar'.split(new RegExp( '\\b' + urlRegex().source + '\\b'));
 // console.log(matchesArray);
-const input = 'go to    www.test.com and http://www.testurl.com';
-const arrays = input.split(/([\s\r\n,]+)/g);
-const result = arrays.map((value: string): string => {
-  if (regex.test(value)) {
-    return '<>' + value + '<>';
-  } else {
-    return value;
-  }
-});
-console.log(result);
+// const input = 'go to    www.test.com and http://www.testurl.com';
+// const arrays = input.split(/([\s\r\n,]+)/g);
+// const result = arrays.map((value: string): string => {
+//   if (regex.test(value)) {
+//     return '<>' + value + '<>';
+//   } else {
+//     return value;
+//   }
+// });
+// console.log(result);
 // const startIndex = regex.lastIndex;
 // const result = regex.exec(input);
 // const nextStartIndex = regex.lastIndex;
