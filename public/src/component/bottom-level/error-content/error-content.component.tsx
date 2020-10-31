2c39ae98d8e58ce50790ad89037f7f99f403488d
Shaw Clinton <tangxiao933@gmail.com>
2020-10-31 13:08:45 -0700

    Create error-content.component.tsx

---
+++ public/src/component/bottom-level/error-content/error-content.component.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { startCase as _startCase } from 'lodash';
+type Props = {
+  severity: 'success' | 'info' | 'warning' | 'error';
+  text: string;
+  title?: string;
+};
+
+const AppAlert = (props: Props): React.ReactElement => {
+  const { text, severity, title = _startCase(severity) } = props;
+  return (
+    <Alert severity={severity}>
+      <AlertTitle>{title}</AlertTitle>
+      {text}
+    </Alert>
+  );
+};
