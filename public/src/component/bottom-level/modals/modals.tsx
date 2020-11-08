d66ae8409faedb4df8de75f3f7ceb823da127103
Shaw Clinton <tangxiao933@gmail.com>
2020-11-08 13:52:53 -0800

    Update modals.tsx

--- public/src/component/bottom-level/modals/modals.tsx
+++ public/src/component/bottom-level/modals/modals.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
-import { ModalActionsPropsGroup } from '../../../service/shared-service/modal/modal-utils';
+import { ModalActionsPropsGroup, ModalType } from '../../../service/shared-service/modal/modal-utils';
 import { ConfirmActionPanel } from './confirmation-modal/confirmation-action-panel.component';
 import { AlertActionPanel } from './alert-modal/alert-action-panel.component';
-
-type ActionComponentPropsUnion = ModalActionsPropsGroup[keyof ModalActionsPropsGroup];
 
 type DialogProps = {
   title: string;
@@ -13,11 +11,14 @@ type DialogProps = {
   isOpen: boolean;
 };
 
-const createModal = <T extends ActionComponentPropsUnion>(ActionComponent: React.FC<T>): React.FC<DialogProps & T> => {
+const createModal = <T extends keyof ModalActionsPropsGroup>(
+  ActionComponent: React.FC<ModalActionsPropsGroup[T]>
+): React.FC<DialogProps & ModalActionsPropsGroup[T]> => {
   console.log(123);
   // eslint-disable-next-line react/display-name
-  return (props: DialogProps & ActionComponentPropsUnion): React.ReactElement => {
+  return (props: DialogProps & ModalActionsPropsGroup[T]): React.ReactElement => {
     const { title, content, isOpen, handleClose, ...rest } = props;
+    const actionComponentProps = (rest as ModalActionsPropsGroup[keyof ModalActionsPropsGroup]) as ModalActionsPropsGroup[T];
     return (
       <Dialog open={isOpen} onClose={handleClose}>
         <DialogTitle>{title}</DialogTitle>
@@ -25,15 +26,15 @@ const createModal = <T extends ActionComponentPropsUnion>(ActionComponent: React
           <DialogContentText id="alert-dialog-description">{content}</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <ActionComponent {...(rest as T)} />
+          <ActionComponent {...actionComponentProps} />
         </DialogActions>
       </Dialog>
     );
   };
 };
 
-const AlertModal = createModal(AlertActionPanel);
-const ConfirmationModal = createModal(ConfirmActionPanel);
+const AlertModal = createModal<ModalType.ALERT>(AlertActionPanel);
+const ConfirmationModal = createModal<ModalType.CONFIRM>(ConfirmActionPanel);
 
 const modalMapping = {
   alert: AlertModal,
