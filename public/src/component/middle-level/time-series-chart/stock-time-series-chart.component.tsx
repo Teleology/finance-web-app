ded56f592627ee99d7f64421189b43e5f8b185f3
Shaw Clinton <tangxiao933@gmail.com>
2020-11-20 22:27:28 -0800

    add custom bread

--- public/src/component/middle-level/time-series-chart/stock-time-series-chart.component.tsx
+++ public/src/component/middle-level/time-series-chart/stock-time-series-chart.component.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { pick } from 'lodash';
 import { connect } from 'react-redux';
+import { Breadcrumbs, Chip, Grid } from '@material-ui/core';
 import { stockTimeSeriesAction } from '../../../service/stock-time-series/stock-time-series.action';
 import { RootState } from '../../../service/root-store';
 import { stockTimeSeriesChartConverter } from '../../../service/stock-time-series/stock-time-series.seletor';
 import { LineChart } from '../../bottom-level/line-chart/line-chart.component';
-import { Breadcrumbs, Grid } from '@material-ui/core';
+import { Breadcrumb } from '../../bottom-level/app-chip.component';
 
 const mapDispatch = pick<typeof stockTimeSeriesAction, 'getTimeSeries'>(stockTimeSeriesAction, ['getTimeSeries']);
 const mapState = ({ stockTimeSeries, companyCollection }: RootState) =>
@@ -21,19 +22,25 @@ const StockTimeSeriesChart = ({ getTimeSeries, series, company }: Props): React.
   React.useEffect(() => {
     company !== null && getTimeSeries(company);
   }, [getTimeSeries, company]);
+
+  const handleBreadCrumbClick = React.useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e.target);
+  }, []);
+
   // set LineChart's debounceTime to 0 if you want immediately updating
   return (
-    <div>
+    <>
       <div>
         <Breadcrumbs>
-
+          <Chip onClick={handleBreadCrumbClick} label="xxxx" inputProps={{ 'data-foo': 'bar' }} />
+          <Breadcrumb label="days" onClick={handleBreadCrumbClick} data-id="days" />
+          <Breadcrumb label="weeks" onClick={handleBreadCrumbClick} data-id="weeks" />
         </Breadcrumbs>
       </div>
-        <div style={{ minHeight: 200, height: '50vh', maxHeight: 800 }}>
-          <LineChart data={series.data} />;
-        </div>
-    </div>
-
+      <div style={{ minHeight: 200, height: '50vh', maxHeight: 800 }}>
+        <LineChart data={series.data} />;
+      </div>
+    </>
   );
 };
 
