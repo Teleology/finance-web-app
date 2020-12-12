d2a6a6d14f897242ef42c86ff87ce13f442938c5
Shaw Clinton <tangxiao933@gmail.com>
2020-12-12 14:23:49 -0800

    add raw tree item

--- public/src/view/container/app-drawer/app-drawer.component.tsx
+++ public/src/view/container/app-drawer/app-drawer.component.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { Business as BusinessIcon, Timeline as TimeLineIcon, Compare as CompareIcon } from '@material-ui/icons';
+import {
+  Business as BusinessIcon,
+  Timeline as TimeLineIcon,
+  Compare as CompareIcon,
+  ExpandMore as ExpandMoreIcon,
+  ChevronRight as ChevronRightIcon
+} from '@material-ui/icons';
+import { TreeView, TreeItem } from '@material-ui/lab';
 import { AppRouteLink } from '../../common/app-link.component';
 import { infoLink, compareLink, searchLink } from '../../../utils/network-util';
 import styles from './app-drawer.styles';
@@ -11,9 +18,9 @@ const AppDrawer = (props: Props): React.ReactElement => {
   const { useContainerStyles, useListItemIconStyles } = styles;
   return (
     <Drawer anchor="left" open={isOpen} onClose={close} classes={useContainerStyles()}>
-      <List component="nav" onClick={close}>
+      <List component="nav">
         <AppRouteLink to={searchLink}>
-          <ListItem button={true}>
+          <ListItem button={true} onClick={close}>
             <ListItemIcon classes={useListItemIconStyles()}>
               <BusinessIcon />
             </ListItemIcon>
@@ -21,7 +28,7 @@ const AppDrawer = (props: Props): React.ReactElement => {
           </ListItem>
         </AppRouteLink>
         <AppRouteLink to={infoLink}>
-          <ListItem button={true}>
+          <ListItem button={true} onClick={close}>
             <ListItemIcon classes={useListItemIconStyles()}>
               <TimeLineIcon />
             </ListItemIcon>
@@ -29,13 +36,22 @@ const AppDrawer = (props: Props): React.ReactElement => {
           </ListItem>
         </AppRouteLink>
         <AppRouteLink to={compareLink}>
-          <ListItem button={true}>
+          <ListItem button={true} onClick={close}>
             <ListItemIcon classes={useListItemIconStyles()}>
               <CompareIcon />
             </ListItemIcon>
             <ListItemText primary="Compare" />
           </ListItem>
         </AppRouteLink>
+        <ListItem>
+          <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />}>
+            <TreeItem nodeId="1" label="Info">
+              <TreeItem nodeId="2" label="Calendar" />
+              <TreeItem nodeId="3" label="Chrome" />
+              <TreeItem nodeId="4" label="Webstorm" />
+            </TreeItem>
+          </TreeView>
+        </ListItem>
       </List>
     </Drawer>
   );
